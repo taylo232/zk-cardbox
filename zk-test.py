dcd4e6474fbdf8c81883bf2c8f7d786f3e143314
rjt <rjt@fastmail.com>
2020-04-07 11:17:32 +0100

    1st iteration

--- zk-test.py
+++ zk-test.py
@@ -1,5 +1,48 @@
 #!/usr/bin/env python3
 
-import networkx as g
+import networkx as nx
+import matplotlib.pyplot as plt   # ass python3-matplotlib
 
-print("ok")
+g = nx.MultiDiGraph()
+g.add_node('fred')
+g.add_node('bill')
+g.add_node('mary')
+g.add_nodes_from(["julie","arthur"])
+
+g.add_edge('fred', 'mary')
+g.add_edge('arthur', 'fred')
+g.add_edge('fred', 'bill')
+g.add_edge('mary', 'julie')
+g.add_edge('mary', 'arthur')
+g.add_edge('bill', 'arthur')
+g.add_edge('wendy', 'fred')
+
+#g = nx.relabel_nodes(g,{'fred':'f r e d'}, copy=False)
+
+
+
+print("=======")
+print('Nodes:', g.nodes())
+print('Edges:', g.edges())
+print('Directed:', nx.is_directed(g))
+print("=======")
+print('Fred ->', g.successors('fred'))
+print('Fred <-', g.predecessors('fred'))
+
+
+print("=======", flush=True)
+
+nx.draw(g, with_labels=True)
+plt.savefig("path_graph1.png")
+plt.show()
+
+
+# dg = nx.DiGraph()
+# dg.add_node('greta')
+# dg.add_node('garbo')
+# dg.add
+
+
+
+
+
